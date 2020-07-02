1bef656968ad107cd3686160e198c4bdfbc9cdfd
최용준 <sunn3842@naver.com>
2020-07-02 15:36:37 +0900

    User 변수 출력시 문구 재가공
    -toString 오버라이딩

--- src/codes/datas/User.java
+++ src/codes/datas/User.java
@@ -17,6 +17,14 @@ public class User {
 		
 	}
 	
+//	변수를 자체로 찍어도 가공된 문구가 나오도록
+	@Override
+	public String toString() {
+//		가공된 양식의 String을 이 메쏘드의 결과로 지정.
+//		String.format으로 가공한 문장을 리턴하자.
+//		조경진(33세) : 010-5112-3237
+		return String.format("%s(%d세) : %s",this.name, this.getKoreanAge(), this.phoneNum);
+	}
 	public User(String name, String phoneNum, int birthYear) {
 		super();
 		this.name = name;
